be620a8cbcc882cf280dee235bd69f37ec2beb11
louis-axel <louisaxelrenard@gmail.com>
2021-01-31 00:13:21 +0100

    init page Accueil

---
+++ Devlab_Actulan/pages/Accueil.vue
@@ -0,0 +1,22 @@
+<template>
+    
+  
+</template>
+
+
+<script>
+
+import LanCalendrier from '../components/LanCalendrier.vue'
+
+
+export default {
+    components:{
+        LanCalendrier
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>

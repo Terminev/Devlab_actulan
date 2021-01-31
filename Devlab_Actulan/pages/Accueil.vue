3fca2d6f4196fcd81b6e63518fe45dc053634dc8
louis-axel <louisaxelrenard@gmail.com>
2021-01-31 01:42:49 +0100

    add 1er article

--- Devlab_Actulan/pages/Accueil.vue
+++ Devlab_Actulan/pages/Accueil.vue
@@ -1,5 +1,30 @@
 <template>
-    
+   <section id="body">
+       <article id="map_bloc">
+        <div id="bloc_gauche">
+           <div  id="paragraphe">
+            <p>
+            NOS PROCHAINES LANS
+           </p>
+           </div>
+           
+           <div id="date">
+            <span>
+            voir les dates
+           </span>
+           <div>
+            <img src="~/assets/img/flechegauche.png" alt="flechegauche">
+           </div>
+           
+           </div>
+           
+       </div>
+       </article>
+       <article>
+
+       </article>
+       
+   </section>
   
 </template>
 
@@ -9,7 +34,11 @@
 import LanCalendrier from '../components/LanCalendrier.vue'
 
 
+
 export default {
+    
+    
+
     components:{
         LanCalendrier
     }
@@ -18,5 +47,59 @@ export default {
 
 
 <style scoped>
+#map_bloc{
+    height: 850px;
+    display: flex;
+    align-items: center;
+    
+
+    background-image: url(~assets/img/mapactulanv2.png);
+    background-repeat: no-repeat;
+
+    background-position-x: -500px;
+}
+#bloc_gauche{
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 876px;
+height: 634px;
+background-color: white;
+
+position: absolute;
+right: 0;
+
+}
+
+#bloc_gauche p{
+width: 465px;
+  height: 300px;
+font-size: 75px;
+  
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  text-align: left;
+  color: #010005;
+
+}
+#date{
+    width: 465px;
+  height: 50px;
+ display: flex;
+ justify-content: space-between;
+
+}
+#date span{
+  font-size: 55px;
+  
+  text-align: left;
+  color: #6e66c8;
+}
+#date img{
+  padding-top: 15px;
+}
 
 </style>

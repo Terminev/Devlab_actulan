3c799d253ad0f262696cf9e4ec9567d03d1ffbfa
sacha <sachagaleuzzi@gmail.com>
2021-01-31 23:56:56 +0100

    ajout page inscription

---
+++ Devlab_Actulan/components/inscription/TitreH3.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="TitreH3">
+        <h3>{{Titre}}</h3>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['Titre']
+}
+</script>
+
+<style  scoped>
+    div {
+        display: flex;
+    }
+
+    div hr {
+        border: 1.5px solid #6E66C8;
+        height: 0px;
+        margin: auto;
+        width: 70%;
+        display: flex;
+        justify-content: flex-end;
+
+    }
+    div h3 {
+        padding-left: 40px;
+        margin-right: 40px;
+        color: #6E66C8;
+        font-size: 50px;
+        font-weight: 600;
+        text-transform: uppercase;
+    }
+</style>

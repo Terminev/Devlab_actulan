74cb43a99113b33224f436b155e1d0205b0782c8
Karen <karen.azoulay@edu.devinci.fr>
2021-01-31 15:40:07 +0100

    component responsive

---
+++ Devlab_Actulan/components/BoutonAmi.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="BoutonMessage">
+    <picture>
+        <img src="../assets/Bouton_Plus.png" alt="">
+    </picture>
+
+    <p>Ajouter en ami</p>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#BoutonMessage{
+    background-color: #6E66C8;
+    width: 250px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    padding: 5px 15px;
+}
+#BoutonMessage p{
+    display: flex;
+    justify-content: center;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 19px;
+    font-weight: bold;
+    color: white;
+}
+#BoutonMessage img{
+    width: 60%;
+}
+
+</style>

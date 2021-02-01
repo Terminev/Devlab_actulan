c7b1c7efd47c27c25f0a304395f3fb3931735692
Alexandre Delafosse <alexandre.delafosse@edu.devinci.fr>
2021-02-01 11:38:57 +0100

    BrancheAlexandreD

--- Devlab_Actulan/pages/connexion.vue
+++ Devlab_Actulan/pages/connexion.vue
@@ -1,87 +1,76 @@
 <template>
-   <div class="connexion-body">
-      <div class="connexion-block">
-        <TitreSection 
-        titre= "CONNEXION"/>
-        <InputForm 
-        PlaceHolder= "PSEUDO"
-        Name= "pseudo"/>
-        <InputForm 
-        PlaceHolder= "MOT DE PASSE"
-        Name= "password"/>
-        <BoutonValider 
-        Text="Connexion"/>
-        <p>OU</p>
-        <Facebook />
-        <NuxtLink id="inscription" to="/">S'INSCRIRE</NuxtLink>
-        </div>
-
-   </div>
+  <div class="connexion-body">
+    <div class="connexion-block">
+      <TitreSection titre="CONNEXION" />
+      <InputForm PlaceHolder="PSEUDO" Name="pseudo" />
+      <InputForm PlaceHolder="MOT DE PASSE" Name="password" />
+      <BoutonValider Text="Connexion" />
+      <p>OU</p>
+      <Facebook />
+      <NuxtLink id="inscription" to="/">S'INSCRIRE</NuxtLink>
+    </div>
+  </div>
 </template>
 
 <script>
-import TitreSection from '~/components/TitreSection'
-import InputForm from '~/components/InputForm'
-import BoutonValider from '~/components/BoutonValider'
-import Facebook from '~/components/Connexion/FacebookConnect'
+import TitreSection from "~/components/TitreSection";
+import InputForm from "~/components/InputForm";
+import BoutonValider from "~/components/BoutonValider";
+import Facebook from "~/components/Connexion/FacebookConnect";
 
 export default {
-  name: 'connexion',
+  name: "connexion",
   components: {
     TitreSection,
     InputForm,
     BoutonValider,
     Facebook,
-  }
-}
-
+  },
+};
 </script>
 
 <style scoped>
 .connexion-body {
   background-image: linear-gradient(161deg, #667eea 15%, #764ba2 70%);
-
 }
-.connexion-block{
+.connexion-block {
   border-radius: 13px;
-  
+
   width: 80%;
   margin: 100px auto;
   background-color: white;
   display: flex;
   align-items: center;
   flex-direction: column;
-
-
 }
-input{
+input {
   width: 50%;
   margin: 10px 0;
 }
-.TitreSection{
+.TitreSection {
   margin-bottom: 70px;
   margin-top: -15px;
 }
-.BoutonValider{
+.BoutonValider {
   margin: 30px 0;
-
 }
-p{
+p {
   font-size: 41px;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   color: #6e66c8;
-  font-weight:bold
+  font-weight: bold;
 }
-.FacebookConnect{
+.FacebookConnect {
   margin: 30px 0px;
 }
-#inscription{
+#inscription {
   color: #4d4d4d;
   font-size: 26px;
   font-weight: bold;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   text-decoration: none;
   margin-bottom: 40px;
 }
-
 </style>

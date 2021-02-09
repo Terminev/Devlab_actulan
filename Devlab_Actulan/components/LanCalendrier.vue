3994143f6125f924c71e936aa0153d39f041ef07
Karen <karen.azoulay@edu.devinci.fr>
2021-02-09 16:16:10 +0100

    composant

--- Devlab_Actulan/components/LanCalendrier.vue
+++ Devlab_Actulan/components/LanCalendrier.vue
@@ -1,103 +1,97 @@
 <template>
-    <div id="etiquette">
-        <p>5/12</p>
-        <p>nantarena 2.0</p>
-        <p>192 places</p>
-    </div>
+  <div>
+    <span>
+      <p>{{ date }}</p>
+      <p>{{ titre }}</p>
+    </span>
+    <p>{{ place }}</p>
+  </div>
 </template>
 
 <script>
-
+  export default{
+    name: 'LanCalendrier',
+    props: ["date", "titre", "place"]
+  }
 </script>
 
 <style scoped>
-    #etiquette {
-        
-        border: black solid 3px;
-        display: flex;
-        flex-wrap: nowrap;
-        align-items: center;
-        background-color: white;
-        width: 80%;
-        height: 70px;
+  div {
+    border: black solid 2px;
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    background-color: white;
+    width: 80%;
+    height: 70px;
+    justify-content: space-between;
+  }
+
+  p {
+    margin-left: 10%;
+    text-transform: uppercase;
+  }
+
+  span{
+    display: flex;
+    align-items: center;
+  }
+
+  span p:first-child {
+    margin-left: 10px;
+    background-color: #6e66c8;
+    padding: 16px 8px;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    border-radius: 50px;
+  }
+
+  p:last-child {
+    margin: 20px;
+  }
+
+  @media screen and (max-width: 1375px) {
+    div {
+      width: 90%;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    p {
+      font-size: 14px;
+    }
+  }
+
+  @media screen and (max-width: 901px) {
+    div {
+      box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+      background-color: #d8d5d5;
+      border: none;
+      width: 70%;
+      height: 40px;
+      border-radius: 13px;
     }
 
     p {
-        margin-left: 10%;
-        text-transform: uppercase;
+      margin: 0;
+      font-size: 14px;
     }
 
-    p:first-child {
-        margin-left: 10px;
-        background-color: #6e66c8;
-        padding: 16px 8px;
-        width: fit-content;
-        color: white;
-        font-size: 16px;
-        font-weight: bold;
-        border-radius: 50px;
+    span p:first-child {
+      margin-left: -15px;
     }
 
     p:last-child {
-        display: flex;
-        justify-content: flex-end;
-        width: 50%;
-        margin-left: 0;
+      display: flex;
+      justify-content: flex-end;
+      margin: 10px;
     }
- @media screen and (max-width: 1375px) {
-        #etiquette {
-        width: 80%;
-       
-    }
-    }
-    @media screen and (max-width: 1024px) {
-        p {
-        font-size: 14px;
-        
-    }
-    }
-    @media screen and (max-width: 901px) {
-        #etiquette {
-        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
-        background-color: #d8d5d5;
-        border:none;
-        display: flex;
-        flex-wrap: nowrap;
-        align-items: center;
-       
-        width: 50%;
-        height: 40px;
-        border-radius: 13px;
-    }
-    p {
-        font-size: 14px;
-        margin-left: 10%;
-        text-transform: uppercase;
-    }
-     p:first-child {
-        margin-left: -15px;
-        background-color: #6e66c8;
-        padding: 16px 8px;
-        width: fit-content;
-        color: white;
-        font-size: 16px;
-        font-weight: bold;
-        border-radius: 50px;
-    }
-    p:last-child {
-        display: flex;
-        justify-content: flex-end;
-        width: 50%;
-        padding-right: 20px;
+  }
 
+  @media screen and (max-width: 500px) {
+    div {
+      width: 100%;
     }
-    }
-    @media screen and (max-width: 650px) {
-        #etiquette {
-        width: 80%;
-       
-    }
-    }
-    
-
+  }
 </style>
--- Devlab_Actulan/pages/Accueil.vue
+++ Devlab_Actulan/pages/Accueil.vue
@@ -1,397 +1,367 @@
 <template>
-   <section id="body">
-       <article id="map_bloc">
-        <div id="bloc_gauche">
-           <div  id="paragraphe">
-            <p>
-           nos prochaines lans
-           </p>
-           </div>
-           
-           <div id="date">
-            <span>
+  <section id="body">
+    <article id="map_bloc">
+      <div id="bloc_gauche">
+        <div id="paragraphe">
+          <p>
+            nos prochaines lans
+          </p>
+        </div>
+        <div id="date">
+          <span>
             voir les dates
-           </span>
-           <div id="fleche">
+          </span>
+          <div id="fleche">
             <img src="~/assets/img/flechegauche.png" alt="flechegauche">
-           </div>
-           
-           </div>
-           
-       </div>
-       </article>
-       <article id="calendar_date">
-           <div id="contour">
-               <div id="calendar">
-                <img src="~/assets/img/calendar.png" alt="flechegauche">
-               </div>
-               
-               <aside id="composantdate">
-                   <lan-calendrier>
-                   </lan-calendrier>
-                   <lan-calendrier>
-                   </lan-calendrier>
-                   <lan-calendrier>
-                   </lan-calendrier>
-                   <lan-calendrier>
-                   </lan-calendrier>
-                    <lan-calendrier>
-                   </lan-calendrier>
-                   
-               </aside>
-           </div>
-       </article>
-       <article id="esport">
-         <div id="txt_btn">
-           <div id="txt">
-           <ul>
-             <li>Créer associations et évènements avec la communauté Actu-Lan !</li>
-             <li>Participer et s'inscrire auprès de diffèrentes associations et évènements !</li>
-             
-           </ul>
-           <ul>
-             <li>Commenter et discuter avec la communauté Actu-Lan !</li>
-             <li>Partager des images de ces moments inoubliables dans la galerie d'images !</li>
-           </ul>
-           </div>
-           
-            <div id="btn">
-            <button>S'incrire</button>
-            </div>
-           
-         </div>
-       </article>
-       
-   </section>
-  
+          </div>
+        </div>
+      </div>
+    </article>
+    <article id="calendar_date">
+      <div id="contour">
+        <div id="calendar">
+          <img src="~/assets/img/calendar.png" alt="flechegauche">
+        </div>
+        <aside id="composantdate">
+          <LanCalendrier date="15/01" titre="Nanatera" place="15 places" />
+          <LanCalendrier date="15/01" titre="Nanatera" place="15 places" />
+          <LanCalendrier date="15/01" titre="Nanatera" place="15 places" />
+          <LanCalendrier date="15/01" titre="Nanatera" place="15 places" />
+        </aside>
+      </div>
+    </article>
+    <article id="esport">
+      <div id="txt_btn">
+        <div id="txt">
+          <ul>
+            <li>Créer associations et évènements avec la communauté Actu-Lan !</li>
+            <li>Participer et s'inscrire auprès de diffèrentes associations et évènements !</li>
+          </ul>
+          <ul>
+            <li>Commenter et discuter avec la communauté Actu-Lan !</li>
+            <li>Partager des images de ces moments inoubliables dans la galerie d'images !</li>
+          </ul>
+        </div>
+        <div id="btn">
+          <button>S'incrire</button>
+        </div>
+      </div>
+    </article>
+  </section>
 </template>
 
 
 <script>
+  import LanCalendrier from '~/components/LanCalendrier.vue';
 
-import LanCalendrier from '../components/LanCalendrier.vue'
-
-
-
-export default {
-    
-    
-
-    components:{
-        LanCalendrier
+  export default {
+    components: {
+      LanCalendrier
     }
-}
+  }
+
 </script>
 
 
 <style scoped>
+  #body {
+    background: linear-gradient(#667EEA, #754DA4, #764BA2);
+  }
 
-#body{
-background: linear-gradient(#667EEA, #754DA4, #764BA2 );
-}
-#map_bloc{
+  #map_bloc {
     height: 850px;
     display: flex;
     align-items: center;
     background-image: url(~assets/img/mapactulanv2.png);
     background-repeat: no-repeat;
     background-position-x: -500px;
-}
-#bloc_gauche{
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: space-evenly;
-width: 45%;
-height: 600px;
-background-color: white;
+  }
 
-position: absolute;
-right: 0;
+  #bloc_gauche {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 45%;
+    height: 600px;
+    background-color: white;
 
-}
-#bloc_gauche #paragraphe{
-  width: 55%;
-}
+    position: absolute;
+    right: 0;
 
-#bloc_gauche p{
-text-transform: uppercase;
- font-size: 4em;
-color: #010005;
-}
-#date{
- width: 55%;
- display: flex;
- justify-content: space-between;
+  }
 
-}
-#date span{
-  font-size: 3.5em;
-  color: #6e66c8;
-}
+  #bloc_gauche #paragraphe {
+    width: 55%;
+  }
 
-#date img{
-  padding-top: 15px;
-}
+  #bloc_gauche p {
+    text-transform: uppercase;
+    font-size: 4em;
+    color: #010005;
+  }
 
-#calendar_date{
+  #date {
+    width: 55%;
+    display: flex;
+    justify-content: space-between;
+
+  }
+
+  #date span {
+    font-size: 3.5em;
+    color: #6e66c8;
+  }
+
+  #date img {
+    padding-top: 15px;
+  }
+
+  #calendar_date {
     padding-top: 270px;
     display: flex;
     justify-content: center;
-    
-  
-}
-#contour{
+  }
+
+  #contour {
     width: 80%;
-  height: 700px;
-  border-radius: 13px;
-  border: solid 1px #707070;
-  background-color: #ffffff;
-  display: flex;
-  
-
-}
-#contour #calendar{
-    width: 50%;
-    height: 80%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-}
-#contour #composantdate{
-    width: 50%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-
-}
-#esport{
-  padding-bottom: 260px;
-  margin-top: 260px;
-  width: 100%;
-  height: 1000px;
-  background-image: url(~assets/img/fondesportactulan.png);
-  background-repeat: no-repeat;
-  background-position-x: -120px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-}
-#txt_btn{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  width: 70%;
-  height: 80%;
-}
-#txt{
-  height: 65%;
-  display: flex;
-  justify-content: space-between;
-padding-left: 100px;
-
-}
-#txt ul{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-}
-#txt li{
-width: 85%;
-
-font-size: 36px;
-font-weight: bold;
-text-align: left;
-color: #dfe3e8;
-}
-#txt li::marker{
-
-/*font-size: 70px;*/
-color: #dfe3e8;
-}
-#btn{
-  width: 45%;
-}
-
-#btn button{
-  width: 100%;
-  height: 60px;
-  border-radius: 20px;
-  border: solid 1px #080000;
-  background-color: #f4f3f3;
-  font-size: 36px;
-  font-weight: bold;
-  text-align: center;
-  color: #020202;
-  text-transform: uppercase;
-}
-
-@media screen and (max-width: 1129px) {
-  #bloc_gauche #paragraphe{
-  width: 55%;
-}
-#bloc_gauche #paragraphe p{
-  font-size: 3em;
-}
-#date span{
-  font-size: 3em;
-  
-}
-#txt{
-  width: 100%;
-}
-#txt ul{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-
-}
-#txt li{
-width: 80%;
-
-font-size: 28px;
-
-}
-}
-
-@media screen and (max-width: 901px) {
-#map_bloc{
-    height: 800px;
-    display: flex;
-    justify-content: flex-end;
-    background-image: url(~assets/img/mapactulanv2.png);
-    background-repeat: no-repeat;
-    background-position-x: -900px;
-    background-position-y: -100px;
-}
-#bloc_gauche{
-  display: initial;
-  top: 350px;
-  justify-content: initial;
-  border-radius: 25px 25px 0% 0%;
-  width: 100%;
-  height: 560px;
-  
-}
-#bloc_gauche #paragraphe{
-  padding-top: 20px;
-  width: 60%;
-  padding-left: 30px;
-}
-#bloc_gauche #paragraphe p{
-  font-size: 60px;
-}
-#date{
- width: 100%;
- display: flex;
- flex-direction: column;
-
-}
-#date span{
-padding-left: 30px;
-
-}
-#date img{
-  padding-left: 300px;
-  display: flex;
-  margin: auto;
- transform: rotate(90deg);
- 
-}
-
-#calendar_date{
-    padding-top:0px;
-    display: flex;
-    justify-content: center; 
-}
-
-#contour{
-    width: 100%;
-    height: 800px;
-    border-radius: 0px;
-    border: none;
+    height: 700px;
+    border-radius: 13px;
+    border: solid 1px #707070;
     background-color: #ffffff;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-#contour #calendar{
+  }
+
+  #contour #calendar {
     width: 50%;
     height: 80%;
-  display: flex;
-}
-#contour #composantdate{
-    width: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #contour #composantdate {
+    width: 50%;
     height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-evenly;
+  }
 
-}
-#esport{
-  height: auto;
-  padding-bottom: 0px;
-  margin-top: 0px;
-  
+  #esport {
+    padding-bottom: 260px;
+    margin-top: 260px;
+    width: 100%;
+    height: 1000px;
+    background-image: url(~assets/img/fondesportactulan.png);
+    background-repeat: no-repeat;
+    background-position-x: -120px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-}
-#txt_btn{
-  width: 100%;
-}
-#txt{
- padding-left: 0;
-  padding-bottom: 60px;
-  width: 80%;
-  height: 100%;
+  #txt_btn {
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
-}
-#txt ul{
-  display: flex;
-  align-items: center;
-}
-#txt li{
-  padding-top: 60px;
-  font-size: 30px;
-}
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 80%;
+  }
 
-#btn{
-  width: 70%;
-  padding-bottom: 30px;
-}
-}
+  #txt{
+    width: 100%;
+  }
 
-@media screen and (max-width: 425px) {
-  #bloc_gauche #paragraphe{
-  width: 55%;
-}
-#bloc_gauche #paragraphe p{
-  font-size: 2.4em;
-}
-#date span{
-  font-size: 2.5em;
-  
-}
+  #txt ul {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    margin: 100px 10px;
+  }
 
-#txt li{
-font-size: 24px;
-width: 90%;
+  #txt li {
+    width: 500px;
+    font-size: 36px;
+    font-weight: bold;
+    text-align: left;
+    color: #dfe3e8;
+  }
 
-}
-#btn button{
-  height: 40px;
-  font-size: 24px;
-  
-}
-}
+  #txt li::marker {
+    color: #dfe3e8;
+  }
 
+  #btn {
+    width: 45%;
+  }
+
+  #btn button {
+    width: 100%;
+    height: 60px;
+    border-radius: 20px;
+    border: solid 1px #080000;
+    background-color: #f4f3f3;
+    font-size: 36px;
+    font-weight: bold;
+    text-align: center;
+    color: #020202;
+    text-transform: uppercase;
+  }
+
+  @media screen and (max-width: 1129px) {
+    #bloc_gauche #paragraphe {
+      width: 55%;
+    }
+
+    #bloc_gauche #paragraphe p {
+      font-size: 3em;
+    }
+
+    #date span {
+      font-size: 3em;
+    }
+
+    #txt {
+      width: 100%;
+    }
+
+    #txt ul {
+      display: block;
+      margin: 0px 10px;
+    }
+
+    #txt li {
+      width: 80%;
+      margin: 40px auto;
+      font-size: 28px;
+    }
+  }
+
+  @media screen and (max-width: 901px) {
+    #map_bloc {
+      height: 800px;
+      display: flex;
+      justify-content: flex-end;
+      background-image: url(~assets/img/mapactulanv2.png);
+      background-repeat: no-repeat;
+      background-position-x: -900px;
+      background-position-y: -100px;
+    }
+
+    #bloc_gauche {
+      display: initial;
+      top: 350px;
+      justify-content: initial;
+      border-radius: 25px 25px 0% 0%;
+      width: 100%;
+      height: 560px;
+
+    }
+
+    #bloc_gauche #paragraphe {
+      padding-top: 20px;
+      width: 60%;
+      padding-left: 30px;
+    }
+
+    #bloc_gauche #paragraphe p {
+      font-size: 60px;
+    }
+
+    #date {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+
+    }
+
+    #date span {
+      padding-left: 30px;
+
+    }
+
+    #date img {
+      padding-left: 300px;
+      display: flex;
+      margin: auto;
+      transform: rotate(90deg);
+
+    }
+
+    #calendar_date {
+      padding-top: 0px;
+      display: flex;
+      justify-content: center;
+    }
+
+    #contour {
+      width: 100%;
+      height: 800px;
+      border-radius: 0px;
+      border: none;
+      background-color: #ffffff;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    #contour #calendar {
+      width: 50%;
+      height: 80%;
+      display: flex;
+    }
+
+    #contour #composantdate {
+      width: 80%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-evenly;
+
+    }
+
+    #esport {
+      height: auto;
+      padding-bottom: 0px;
+      margin-top: 0px;
+
+
+    }
+
+    #txt {
+      padding: 100px 0;
+    }
+  }
+
+  @media screen and (max-width: 425px) {
+    #bloc_gauche #paragraphe {
+      width: 55%;
+    }
+
+    #bloc_gauche #paragraphe p {
+      font-size: 2.4em;
+    }
+
+    #date span {
+      font-size: 2.5em;
+
+    }
+
+    #txt li {
+      font-size: 24px;
+      width: 90%;
+
+    }
+
+    #btn button {
+      height: 40px;
+      font-size: 24px;
+
+    }
+  }
 
 </style>
